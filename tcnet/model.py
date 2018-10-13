8c169bdad45b671a96a81e8c93f8a702530b0423
xadrianzetx <37713008+xadrianzetx@users.noreply.github.com>
2018-10-13 14:22:18 +0200

    Update model.py

--- tcnet/model.py
+++ tcnet/model.py
@@ -1,6 +1,6 @@
 import numpy as np
 from keras.models import Model, Sequential, load_model
-from keras.layers import Conv1D, BatchNormalization, SpatialDropout1D, Input, add, Activation
+from keras.layers import Conv1D, BatchNormalization, Input, Activation, add
 from keras.optimizers import Adam
 from keras.utils import CustomObjectScope
 from tcnet.activations import gated_activation
@@ -21,16 +21,14 @@ class TemporalConvNet:
 
         # Residual block
         c1 = Conv1D(self._n_filters, kernel_size=4, strides=1, padding='causal', dilation_rate=dilation)(inputs)
-        n1 = BatchNormalization(momentum=0.6)(c1)
-        a1 = Activation(gated_activation)(n1)
-        d1 = SpatialDropout1D(rate=0.2)(a1)
-        c2 = Conv1D(self._n_filters, kernel_size=4, strides=1, padding='causal', dilation_rate=dilation)(d1)
-        n2 = BatchNormalization(momentum=0.6)(c2)
-        a2 = Activation(gated_activation)(n2)
-        d2 = SpatialDropout1D(rate=0.2)(a2)
+        a1 = Activation(gated_activation)(c1)
+        n1 = BatchNormalization(momentum=0.6)(a1)
+        c2 = Conv1D(self._n_filters, kernel_size=4, strides=1, padding='causal', dilation_rate=dilation)(n1)
+        a2 = Activation(gated_activation)(c2)
+        n2 = BatchNormalization(momentum=0.6)(a2)
 
         # Residual connection
-        residual = Conv1D(1, kernel_size=1, padding='same')(d2)
+        residual = Conv1D(1, kernel_size=1, padding='same')(n2)
         outputs = add([inputs, residual])
 
         return Model(inputs=inputs, outputs=outputs, name='residual_block_{}'.format(factor))
@@ -44,14 +42,13 @@ class TemporalConvNet:
 
         return model
 
-    def train(self, train_x, train_y, val_x, val_y, epochs, verbose):
+    def train(self, train_x, train_y, epochs, verbose, x_test):
         return self._model.fit(
             x=train_x,
             y=train_y,
             batch_size=self._seq_length,
             epochs=epochs,
-            verbose=verbose,
-            validation_data=(val_x, val_y)
+            verbose=verbose
         )
 
     def predict(self, data, n_ahead):
